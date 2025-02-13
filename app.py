7a6458a06e319ae2bb1d9b8668829a4e0dcd6201
shantanu <shantanu0201singh@gmail.com>
2025-02-13 19:08:50 +0530

    first of last pushes

---
+++ app.py
@@ -0,0 +1,223 @@
+from joblib import load
+from flask import Flask, request, jsonify, render_template
+from model_helper import load_model_and_preprocessors,get_top_recommendations,predict_with_model,load_nlp_models,find_relevant_text,get_embedding,load_segmentation_model,create_cluster_only_graph,predict_image
+import numpy as np
+import traceback
+import pandas as pd
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import traceback
+import plotly.graph_objects as go
+import io
+from PIL import Image
+# Load the saved model
+
+# Initialize Flask app
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/calories')
+def calories_page():
+    return render_template('calories.html')
+
+@app.route('/diet')
+def diet_page():
+    return render_template('diet.html')
+
+@app.route('/exercise')
+def exercise_page():
+    return render_template('exercise.html')
+
+@app.route('/predict_calories', methods=['POST'])
+def predict_calories():
+    try:
+        data = request.json
+        gender = data['gender']  # 0 or 1
+        heartrate = data['heartrate']
+        body_temp = data['body_temp']
+        height = data['height']
+        age = data['age']
+        
+        inputs = [[float(gender), float(age), float(height), float(heartrate), float(body_temp)]]
+        model = load("models/calories/xgr_calories.joblib")
+
+        calories = model.predict(inputs)[0]
+        
+        return jsonify({'calories': float(calories)})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/predict_diet', methods=['POST'])
+def predict_diet():
+    try:
+        data = request.json
+        inputs =np.asarray( [[data['Age'], data['Height'], data['Weight'], data['BMI'],
+                  data['Sex'], data['Level'], data['Fitness Goal'], data['Fitness Type'],
+                  data['Hypertension'], data['Diabetes']]])
+        path="models/gym_diet"
+        model_name="diet"
+        model, mlb, scaler,label_dict=load_model_and_preprocessors(path,model_name)
+        diet_recommendations = predict_with_model(model, scaler, mlb, inputs, top_n=3,label_encoder=label_dict)
+        final=diet_recommendations[0]
+        out=""
+        for i in final:
+            out=out+"    "+i
+        
+        
+        return jsonify({'diet_recommendation': out})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/predict_exercise', methods=['POST'])
+def predict_exercise():
+    try:
+        data = request.json
+        inputs = np.asarray([[data['Age'], data['Height'], data['Weight'], data['BMI'],
+                  data['Sex'], data['Level'], data['Fitness Goal'], data['Fitness Type'],
+                  data['Hypertension'], data['Diabetes']]])
+        print(inputs.shape)
+        path="models/gym_diet"
+        model_name="exercise"
+        model, mlb, scaler,label_dict=load_model_and_preprocessors(path,model_name)
+        exer_recommendations = predict_with_model(model, scaler, mlb, inputs, top_n=3,label_encoder=label_dict)
+        final=exer_recommendations[0]
+        out=""
+        for i in final:
+            out=out+"    "+i
+        print("Diet recommendations for sample inputs:", out)
+        
+        
+        return jsonify({'exercise_recommendation': out})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/segmentation')
+def segmentation_page():
+    """Renders segmentation.html with the default cluster-only graph."""
+    graph_html = create_cluster_only_graph()
+    return render_template('segmentation.html', graph_html=graph_html)
+
+@app.route('/predict_segment', methods=['POST'])
+def predict_segment():
+    try:
+        data = request.json
+        kmeans_model,scaler, df_grouped=load_segmentation_model()
+
+        # Extract user inputs and create DataFrame
+        columns = ['TotalSteps', 'LoggedActivitiesDistance', 'ModeratelyActiveDistance',
+                   'LightActiveDistance', 'SedentaryActiveDistance', 'SedentaryMinutes', 'Calories']
+        user_inputs_df = pd.DataFrame([[data[col] for col in columns]], columns=columns)
+
+        # Scale user inputs
+        user_scaled = scaler.transform(user_inputs_df)
+
+        # Predict cluster
+        cluster_label = kmeans_model.predict(user_scaled)[0]
+
+        # Convert distances from miles to meters for display
+        distance_columns = ["LoggedActivitiesDistance", "ModeratelyActiveDistance",
+                            "LightActiveDistance", "SedentaryActiveDistance"]
+        user_inputs_df[distance_columns] *= 1609.34  
+
+        # Create radar chart (Clusters + User Data)
+        categories = df_grouped.columns.tolist()
+        fig = go.Figure()
+
+        # Plot clusters
+        for cluster in df_grouped.index:
+            fig.add_trace(go.Scatterpolar(
+                r=df_grouped.loc[cluster].values,  
+                theta=categories,  
+                fill='toself',
+                name=f'Cluster {cluster}',
+                opacity=0.5
+            ))
+
+        # Plot user input
+        fig.add_trace(go.Scatterpolar(
+            r=user_inputs_df.iloc[0].values,  # Use DataFrame values directly
+            theta=categories,  
+            fill='none',  
+            name="User Progress",
+            line=dict(color='black', width=3, dash='solid'),
+            marker=dict(color='black', size=6)
+        ))
+
+        fig.update_layout(
+            polar=dict(radialaxis=dict(visible=True, gridcolor="lightgrey")),
+            title="User Segmentation - Cluster Comparison",
+            showlegend=True,
+            font=dict(size=14)
+        )
+
+        
+
+        # Convert to HTML
+        graph_html = fig.to_html(full_html=False)
+        if cluster_label==0:
+            cluster_label="Light Mover"
+        else:
+            cluster_label="Pro Mover"
+        # Return JSON response (not render_template)
+        #return jsonify({'graph_html': graph_html, 'cluster_name': cluster_label})
+        return render_template('segementation_result.html', graph_html=graph_html, cluster_name=cluster_label)
+
+    except Exception as e:
+        error_message = str(e)
+        error_traceback = traceback.format_exc()
+        print(error_traceback)
+        return jsonify({'error': error_message, 'traceback': error_traceback}), 400
+
+@app.route('/FactCheck')
+def factcheck_page():
+    return render_template('factcheck.html')
+
+@app.route('/fact_check', methods=['POST'])
+def fact_check():
+    try:
+        data = request.json
+        user_question = data['question']
+        model, tokenizer,chunk_embeddings,text_chunks=load_nlp_models()
+        
+        best_match, score = find_relevant_text(user_question,text_chunks,chunk_embeddings,get_embedding,model,tokenizer)
+        print(score)
+        print(best_match)
+
+        return jsonify({'best_match': best_match, 'score': round(float(score), 2)})
+    except Exception as e:
+        error_message = str(e)
+        error_traceback = traceback.format_exc()
+        print(error_traceback)
+        return jsonify({'error': error_message, 'traceback': error_traceback}), 400
+@app.route('/ExerciseClassification')
+def exercise_classification_page():
+    return render_template('exerciseclassification.html')
+@app.route('/predict_exercise_class', methods=['POST'])
+def predict_exercise_class():
+    try:
+        if 'image' not in request.files:
+            return jsonify({'error': 'No image uploaded'}), 400
+        
+        image = request.files['image']
+        if image.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+        image_stream = io.BytesIO(image.read())
+        pil_image = Image.open(image_stream).convert("RGB")  # Ensure RGB format
+        pil_image.save("temp.png")
+        
+        
+        prediction = predict_image(image_path="temp.png")
+        
+        return jsonify({'class': str(prediction)})
+    except Exception as e:
+        error_message = str(e)
+        error_traceback = traceback.format_exc()
+        print(error_traceback)
+        return jsonify({'error': error_message, 'traceback': error_traceback}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model_helper.py
@@ -0,0 +1,155 @@
+import numpy as np
+import joblib
+import json
+from tensorflow.keras.models import load_model  # For loading Keras models
+from transformers import AutoTokenizer, TFAutoModel
+from numpy.linalg import norm
+from joblib import load
+import pandas as pd
+import joblib
+import plotly.graph_objects as go
+from tensorflow.keras.preprocessing.image import load_img,img_to_array
+import tensorflow as tf
+import pickle
+def load_model_and_preprocessors(path,model_name):
+    preprocessor_path=path+"/preprocessor/"
+    model_path=path+"/neural_nets/{}_model.h5".format(model_name)
+    model = load_model(model_path)
+    scaler=joblib.load(preprocessor_path+"scaler.pkl")
+    with open(preprocessor_path+"model_paths.json", "r") as file:
+        label_dict = json.load(file) 
+    for name,file_name in label_dict.items():
+        label_dict[name]=joblib.load(preprocessor_path+file_name)
+    mlb=joblib.load(preprocessor_path+"mlb_"+model_name+".pkl")
+
+    #mlb = joblib.load(model_path + "_mlb.pkl")
+    #scaler = joblib.load(model_path + "_scaler.pkl")
+    return model, mlb, scaler,label_dict
+
+
+
+
+def get_top_recommendations(predictions, mlb, top_n=3):
+    recommended_items = []
+    for pred in predictions:
+        top_indices = np.argsort(pred)[-top_n:][::-1]  # Get indices of top N values
+        top_items = [mlb.classes_[i] for i in top_indices if pred[i] > 0.5]  # Include only if above threshold
+        recommended_items.append(top_items)
+    return recommended_items
+def predict_with_model(model, scaler, mlb, input_data,label_encoder, top_n=3):
+    scaler_data=np.asarray([input_data[0][:4]])
+    input_data_scaled = scaler.transform(scaler_data)
+    label_data=input_data[0][4:]
+    label_data_encoded=[]
+    count=0
+    for key,value in label_encoder.items():
+        data_point=np.asarray([label_data[count]])
+        label_data_encoded.append(value.transform(data_point))
+        count=count+1
+    label_data_encoded=[i[0] for i in label_data_encoded]
+    ids=list(input_data_scaled[0])
+    ids.extend(label_data_encoded)
+    final_data=np.asarray(ids)
+    final_data=final_data.reshape(1,10)
+    print("****")
+    print(final_data)
+    predictions = model.predict(final_data)
+    return get_top_recommendations(predictions, mlb, top_n)
+def load_segmentation_model():
+    # Load KMeans model and scaler
+    kmeans_model = load("models/user_segmentation/kmeans.pkl")
+    scaler = load("models/user_segmentation/scaler.pkl")
+    # Load cluster centroids
+    df_grouped = pd.read_csv("models/user_segmentation/cluster_centroid.csv", index_col=0)
+    # Scale TotalSteps and Calories for display
+    df_grouped["TotalSteps"] /= 1
+    df_grouped["Calories"] /= 1
+    # Convert miles to meters
+    for col in ["LoggedActivitiesDistance", "ModeratelyActiveDistance", 
+            "LightActiveDistance", "SedentaryActiveDistance"]:
+        df_grouped[col] *= 1609.34
+    return kmeans_model,scaler, df_grouped
+def create_cluster_only_graph():
+    """Generates a radar chart with only cluster means (default view)."""
+    kmeans_model,scaler, df_grouped=load_segmentation_model()
+
+    categories = df_grouped.columns.tolist()
+    fig = go.Figure()
+
+    for cluster in df_grouped.index:
+        fig.add_trace(go.Scatterpolar(
+            r=df_grouped.loc[cluster].values,  
+            theta=categories,  
+            fill='toself',
+            name=f'Cluster {cluster}',
+            opacity=0.5
+        ))
+
+    fig.update_layout(
+        polar=dict(radialaxis=dict(visible=True, gridcolor="lightgrey")),
+        title="User Segmentation - Cluster Comparison",
+        showlegend=True,
+        font=dict(size=14)
+    )
+
+    return fig.to_html(full_html=False)
+
+
+def load_nlp_models():
+    # Load model and tokenizer
+    model = TFAutoModel.from_pretrained("models/nlp/bert_model")
+    tokenizer = AutoTokenizer.from_pretrained("models/nlp/bert_tokenizer")
+    chunk_embeddings = np.load("models/nlp/embeddings.npy")
+    text_chunks = np.load("models/nlp/text_chunks.npy")
+
+    return model, tokenizer,chunk_embeddings,text_chunks
+
+def find_relevant_text(user_question, text_chunks, chunk_embeddings, get_embedding,model,tokenizer):
+    # Convert user question into an embedding
+    question_embedding = get_embedding(user_question,model,tokenizer).flatten()
+
+    # Compute cosine similarity
+    similarities = np.dot(chunk_embeddings, question_embedding)
+
+    # Ensure similarity shape matches the number of text chunks
+    assert similarities.shape[0] == len(text_chunks), "Mismatch in similarity scores and text chunks!"
+
+    # Normalize (avoid division by zero)
+    chunk_norms = norm(chunk_embeddings, axis=1)
+    question_norm = norm(question_embedding)
+    similarities = similarities / (chunk_norms * question_norm + 1e-8)
+
+
+
+    # Get the best matching chunk
+    best_match_index = np.argmax(similarities)
+
+    # Ensure index is within bounds
+    if best_match_index >= len(text_chunks):
+        raise IndexError(f"Index {best_match_index} is out of bounds! Check text chunk and embedding sizes.")
+
+    best_match_score = similarities[best_match_index]
+    return text_chunks[best_match_index], best_match_score
+def get_embedding(text,model,tokenizer):
+    inputs = tokenizer(text, return_tensors="tf", padding=True, truncation=True)
+    outputs = model(**inputs)
+    return np.mean(outputs.last_hidden_state.numpy(), axis=1) 
+def predict_image(image_path, model_path="models/image_classifer/cnn_posture_classifier.h5", preprocessor_path="models/image_classifer/preprocessor.pkl", class_labels_path="models/image_classifer/categories.npy"):
+    model = tf.keras.models.load_model(model_path)
+    with open(preprocessor_path, "rb") as f:
+        preprocessor = pickle.load(f)
+    
+    img = load_img(image_path, target_size=(128, 128))
+    img_array = img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocessor.standardize(img_array)
+    
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction, axis=1)[0]
+    class_labels=np.load(class_labels_path)
+    print(class_labels)
+    print(predicted_class)
+    class_name = class_labels[predicted_class] 
+    
+    print(f"Predicted class: {class_name}")
+    return class_name
